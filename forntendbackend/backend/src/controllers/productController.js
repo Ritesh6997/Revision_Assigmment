96d7b6ef0fb6441c360c504011bee0ae045a6ddb
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-07 01:11:16 +0530

    updatedBackend

--- forntendbackend/backend/src/controllers/productController.js
+++ forntendbackend/backend/src/controllers/productController.js
@@ -9,14 +9,24 @@ router.get("/", async (req, res) => {
         const page = req.query.page || 1;
         const limit = 10;
         const skip = (page - 1) * limit;
+        console.log(req.query)
+        // sorting
         const sorting = req.query.sort.split(",");
-        console.log(sorting[0],sorting[1]);
         let sortby = sorting[0] || "_id";
         let sortorder = +(sorting[1]);
-        console.log(sortorder);
-        console.log(sortorder, sortby);
-        const count = await Products.count({});
-        const products = await Products.find().sort({[sortby]:sortorder }).skip(skip).limit(limit).lean().exec();
+        // fitering 
+        const fitering = req.query.filter.split(",");
+        const searching = req.query.search.split(",");
+        let filterby = fitering[0] || "";
+        let filterorder = fitering[1] || ""; 
+        if (searching[1] !== "") {
+            console.log(2)
+            filterby = searching[0];
+           filterorder = searching[1]; 
+        }
+        console.log(filterby,filterorder)
+        const count = await Products.find({[filterby]:filterorder}).count({});
+        const products = await Products.find({[filterby]:filterorder}).sort({[sortby]:sortorder }).skip(skip).limit(limit).lean().exec();
         return res.status(200).send({
             "Products": products,
             "count":Math.ceil(count/limit)});
