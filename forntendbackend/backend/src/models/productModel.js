37b868573f8051b5963e0b9f1726c79633b00e55
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-06 12:56:38 +0530

    updated

---
+++ forntendbackend/backend/src/models/productModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+    id: { type: Number, required: false },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    category: { type: String, enum: ["men's clothing", "women's clothing"], required: true },
+    image: { type: String, required: true },
+    rating: {
+        rate: { type: Number, required: true },
+        count: { type: Number, required: true }
+    }
+});
+
+const Products = mongoose.model("product");
+
+module.exports = Products;
