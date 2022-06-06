3804636c3472949d1e1100249dcadf53e8ba51c4
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-06 12:34:10 +0530

    updated

--- Resturent_nearBy/src/controllers/ResturentController.js
+++ Resturent_nearBy/src/controllers/ResturentController.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const Resturent = require("../models/resturentModel");
+const Restaurant = require("../models/resturentModel");
 
 router.get("/", async (req, res) => {
     try {
-        const resturent = await Resturent.find().lean().exec();
-        return res.status(200).send({ "resturent": resturent });
+        const resturant = await Restaurant.find().lean().exec();
+        return res.status(200).send({ "resturant": resturant });
     } catch (error) {
         return res.status(500).send({ "error": error.message });
     }
 });
-
+router.post("/distance", async (req, res) => {
+    try {
+        let longitude = req.body.longitude;
+        let latitude = req.body.latitude;
+        const resturant =await Restaurant.find({ location:
+   { $geoWithin:
+      { $centerSphere: [ [longitude, latitude], req.body.radius/ 3963.2 ] } } })
+        return res.status(200).send({ "resturant": resturant });
+    } catch (error) {
+        return res.status(500).send({ "error": error.message });
+    }
+});
+router.post("/nearestdistance", async (req, res) => {
+    try {
+        let longitude = req.body.longitude;
+        let latitude = req.body.latitude;
+        let METERS_PER_MILE = 1609.34
+        const resturant = await Restaurant.find({ location: { $nearSphere: { $geometry: { type: "Point", coordinates: [longitude, latitude] }, $maxDistance: 5 * METERS_PER_MILE } } });
+        return res.status(200).send({ "resturant": resturant });
+    } catch (error) {
+        return res.status(500).send({ "error": error.message });
+    }
+});
 router.post("/", async (req, res) => {
     try {
-        const resturent = await Resturent.create(req.body);
-        return res.status(201).send({ "resturent": resturent });
+        const resturant = await Restaurant.create(req.body);
+        return res.status(201).send({ "resturant": resturant });
     } catch (error) {
         return res.status(500).send({ "error": error.message });
     }
--- Resturent_nearBy/src/controllers/neighbourhoodsController.js
+++ Resturent_nearBy/src/controllers/neighbourhoodsController.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Neighborhood = require("../models/neighbourhoods");
-
+const Restaurents = require("../models/resturentModel");
 router.get("/", async (req, res) => {
     try {
         const neighborhood = await Neighborhood.find().lean().exec();
         return res.status(200).send({ "neighborhood": neighborhood });
+    } catch (error) {
+        return res.status(500).send({ "error": error.message });
+    }
+});
+router.post("/NearestNeighborhood", async (req, res) => {
+    try {
+        let longitude = req.body.longitude;
+        let latitude = req.body.latitude;
+        const neighborhood=await Neighborhood.findOne({ geometry: { $geoIntersects: { $geometry: { type: "Point", coordinates: [longitude, latitude] } } } });
+        return res.status(200).send({ "neighborhood": neighborhood });
+    } catch (error) {
+        return res.status(500).send({ "error": error.message });
+    }
+})
+
+router.post("/resturents", async (req, res) => {
+    try {
+        let longitude = req.body.longitude;
+        let latitude = req.body.latitude;
+        const neighborhood = await Neighborhood.findOne({ geometry: { $geoIntersects: { $geometry: { type: "Point", coordinates: [longitude, latitude] } } } });
+        const restaurants = await Restaurents.find( { location: { $geoWithin: { $geometry: neighborhood.geometry } } } ).count()
+        return res.status(200).send({ "neighborhood": restaurants });
     } catch (error) {
         return res.status(500).send({ "error": error.message });
     }
