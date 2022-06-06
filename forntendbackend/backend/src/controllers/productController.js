5459bc9cc76f3a60392461f86ac83958be64528b
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-07 00:18:34 +0530

    updated

--- forntendbackend/backend/src/controllers/productController.js
+++ forntendbackend/backend/src/controllers/productController.js
@@ -9,9 +9,17 @@ router.get("/", async (req, res) => {
         const page = req.query.page || 1;
         const limit = 10;
         const skip = (page - 1) * limit;
-        const filter = req.query.filter;
-        const products = await Products.find().skip(skip).limit(limit).lean().exec();
-        return res.status(200).send({ "Products": products });
+        const sorting = req.query.sort.split(",");
+        console.log(sorting[0],sorting[1]);
+        let sortby = sorting[0] || "_id";
+        let sortorder = +(sorting[1]);
+        console.log(sortorder);
+        console.log(sortorder, sortby);
+        const count = await Products.count({});
+        const products = await Products.find().sort({[sortby]:sortorder }).skip(skip).limit(limit).lean().exec();
+        return res.status(200).send({
+            "Products": products,
+            "count":Math.ceil(count/limit)});
     } catch (error) {
         return res.status(500).send({ "error": error.message });
     }
