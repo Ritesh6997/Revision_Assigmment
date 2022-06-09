04249b73798750c9cc38925c443b5c989bd9c517
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-10 03:07:27 +0530

    updated frontend part

--- EcomerceDesign/frontend/ecomerce/src/components/Navbar.jsx
+++ EcomerceDesign/frontend/ecomerce/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Stack ,IconButton} from "@mui/material"
+import { AppBar, Toolbar, Typography, Stack ,IconButton,Menu,MenuItem} from "@mui/material"
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./navbar.css"
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="sticky">
       <Toolbar
@@ -56,11 +64,58 @@ export default function Navbar() {
             </Link>
           </Typography>
           <Typography variant="h6" component="span">
-            <IconButton color="inherit">
+            <IconButton
+              size="small"
+              sx={{ ml: 2 }}
+              onClick={handleClick}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              color="inherit"
+            >
               <AccountCircleIcon></AccountCircleIcon>
             </IconButton>
           </Typography>
         </Stack>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>MyAccount</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
--- EcomerceDesign/frontend/ecomerce/src/components/Product.jsx
+++ EcomerceDesign/frontend/ecomerce/src/components/Product.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import {Link} from 'react-router-dom'
 import { Button } from '@mui/material'
+import MediaCard from './Card';
 export default function Product() {
+  
   return (
     <div>
       <Link to={"/Addproduct"}>
         <Button>Add Product</Button>
       </Link>
+      <Box
+        sx={{
+          flexGrow: 1,
+        }}
+        width={"100%"}
+      >
+        <Grid justifyContent={"center"} container spacing={2}>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+          <Grid item spacing={3}>
+            <MediaCard></MediaCard>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
+
+
